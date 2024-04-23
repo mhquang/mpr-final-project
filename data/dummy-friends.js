2ae641810a95a0e97b091f5cab92e9f44dbe03f3
Buiduc03 <bduc21112003@gmail.com>
2024-04-23 19:25:30 +0700

    add friend data, add FriendItem component

---
+++ data/dummy-friends.js
@@ -0,0 +1,38 @@
+export const friends = [
+    {
+        name: 'John',
+        age: 10,
+        gender: 'male',
+        hobbies: ['football', 'basketball']
+    },
+    {
+        name: 'Jack',
+        age: 10,
+        gender: 'male',
+        hobbies: ['football', 'basketball']
+    },
+    {
+        name: 'John',
+        age: 10,
+        gender: 'male',
+        hobbies: ['football', 'basketball']
+    },
+    {
+        name: 'John',
+        age: 10,
+        gender: 'male',
+        hobbies: ['football', 'basketball']
+    },
+    {
+        name: 'John',
+        age: 10,
+        gender: 'male',
+        hobbies: ['football', 'basketball']
+    },
+    {
+        name: 'John',
+        age: 10,
+        gender: 'male',
+        hobbies: ['football', 'basketball']
+    },
+];
