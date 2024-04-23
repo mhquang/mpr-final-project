1224a07d3ef7db8f5a867a053b02f4aeaa7a375c
7thDec <megavox03@03gmail.03gmailcom>
2024-04-23 21:46:20 +0700

    Update SideJobScreen.js

---
+++ data/dummy-works.js
@@ -0,0 +1,307 @@
+export const side_job = [{
+    name: "Waiter",
+    requirements: [
+        "At least 16 years old",
+    ],
+    time: "1 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    happiness: { isIncrease: false, index: "3" },
+    money: "Free",
+},
+{
+    name: "Shipper",
+    requirements: [
+        "At least 18 years old",
+    ],
+    time: "2-3 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    happiness: { isIncrease: false, index: "3" },
+    money: "Free",
+},
+{
+    name: "Tutor",
+    requirements: [
+        "At least 18 years old",
+    ],
+    time: "2 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: true, index: "3" },
+    happiness: { isIncrease: false, index: "3" },
+    money: "Free",
+},
+{
+    name: "Make Money Online",
+    requirements: [
+        "At least 18 years old",
+    ],
+    time: "2-3 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: true, index: "3" },
+    happiness: { isIncrease: false, index: "3" },
+    money: "Free",
+},
+{
+    name: "Pan Handler",
+    requirements: [
+        "At least 16 years old",
+    ],
+    time: "1 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "Free",
+}]
+
+export const main_job = [{
+    name: "Software Developer/Engineer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree",
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$60000/year",
+},
+{
+    name: "Web Developer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree",
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$50000/year",
+},
+{
+    name: "Mobile Developer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree",
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$60000/year",
+},
+{
+    name: "Security Engineer",
+    requirements: [
+        "Cybersecurity degree",
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$75000/year",
+},
+{
+    name: "Information Security Analyst",
+    requirements: [
+        "Cybersecurity degree",
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$70000/year",
+},
+{
+    name: "Business Analyst",
+    requirements: [
+        "Information Systems and Technology degree",
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$65000/year",
+},
+{
+    name: "Database Administrator",
+    requirements: [
+        "Information Systems and Technology degree",
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$65000/year",
+},
+{
+    name: "Quality Assurance Engineer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$60000/year",
+},
+{
+    name: "DevOps Engineer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree at least 95 IQ",
+        "At least 95 IQ"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$150000/year",
+},
+{
+    name: "Frontend Developer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$65000/year",
+},
+{
+    name: "Backend Developer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$70000/year",
+},
+{
+    name: "Full-stack Developer",
+    requirements: [
+        "Computer Science degree",
+        "Software Engineering degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$70000/year",
+},
+{
+    name: "Data Analyst",
+    requirements: [
+        "Data Science degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$55000/year",
+},
+{
+    name: "Data Scientist",
+    requirements: [
+        "Data Science degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$80000/year",
+},
+{
+    name: "Data Engineer",
+    requirements: [
+        "Data Science degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$80000/year",
+},
+{
+    name: "Game Developer",
+    requirements: [
+        "Game Development degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$60000/year",
+},
+{
+    name: "Game Designer",
+    requirements: [
+        "Game Development degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$55000/year",
+},
+{
+    name: "Game Tester",
+    requirements: [
+        "Game Development degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$45000/year",
+},
+{
+    name: "AI Engineer",
+    requirements: [
+        "Artificial Intelligence degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$80000/year",
+},
+{
+    name: "Machine Learning Engineer",
+    requirements: [
+        "Artificial Intelligence degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$90000/year",
+},
+{
+    name: "AI Research Scientist",
+    requirements: [
+        "Artificial Intelligence degree"
+    ],
+    time: "12 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$90000/year",
+},
+]
+export const crime = [{
+    name: "Drug Dealer",
+    requirements: [
+        "At least 18 years old"
+    ],
+    time: "1 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$2000 - $10000",
+},
+{
+    name: "Hacking",
+    requirements: [
+        "Cybersecurity degree"
+    ],
+    time: "2 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$1000",
+},
+{
+    name: "Robbery",
+    requirements: [
+        "At least 16 years old"
+    ],
+    time: "1 mins",
+    health: { isIncrease: false, index: "3" },
+    iq: { isIncrease: false, index: "3" },
+    money: "$10 - $1000",
+},
+
+]
