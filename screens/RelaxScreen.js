863f4093bad5d32066699805f50808885b65759d
7thDec <megavox03@03gmail.03gmailcom>
2024-04-24 21:23:38 +0700

    Update RelaxScreen.js

--- screens/RelaxScreen.js
+++ screens/RelaxScreen.js
@@ -2,12 +2,15 @@ import { ScrollView } from "react-native";
 import SportsScreen from "./Relax/SportsScreen";
 import ScreenLayout from "./GeneralComs/ScreenLayout";
 import LeisureScreen from "./Relax/LeisureScreen";
+import TravelScreen from "./Relax/TravelScreen";
+
 function RelaxScreen() {
   return (
     <ScreenLayout>
       <ScrollView>
         <LeisureScreen />
         <SportsScreen />
+        <TravelScreen />
       </ScrollView>
     </ScreenLayout>
   );
