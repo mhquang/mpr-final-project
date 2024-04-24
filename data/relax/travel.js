9cb985bea94af26284c6a1bdb0fa2193217a2441
7thDec <megavox03@03gmail.03gmailcom>
2024-04-24 21:24:20 +0700

    Update TravelScreen.js

---
+++ data/relax/travel.js
@@ -0,0 +1,29 @@
+export const travel = [
+    {
+        name: 'Hue',
+        money: '$100',
+        time: '3 days',
+        requirements: ["At least 10 years old"],
+        happiness: { isIncrease: true, index: "1" },
+        iq: { isIncrease: true, index: "1" },
+        health: { isIncrease: false, index: "1" },
+    },
+    {
+        name: 'Da Nang',
+        money: '$100',
+        time: '3 days',
+        requirements: ["At least 10 years old"],
+        happiness: { isIncrease: true, index: "1" },
+        iq: { isIncrease: true, index: "1" },
+        health: { isIncrease: false, index: "1" },
+    },
+    {
+        name: 'Ha Long',
+        money: '$100',
+        time: '3 days',
+        requirements: ["At least 10 years old"],
+        happiness: { isIncrease: true, index: "1" },
+        iq: { isIncrease: true, index: "1" },
+        health: { isIncrease: false, index: "1" },
+    },
+]
