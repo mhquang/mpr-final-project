4540ef7b53f259cd2aa86449908b2a5d7908759f
mhquang <mhquang03@gmail.com>
2024-04-27 14:40:34 +0700

    Update How to play screen

--- screens/Auth/HowToPlayScreen.js
+++ screens/Auth/HowToPlayScreen.js
@@ -8,7 +8,8 @@ import MaterialIcons from "react-native-vector-icons/MaterialIcons";
 function HowToPlayScreen() {
   const [fontsLoaded] = useFonts({
     Oddval: require("../../assets/fonts/oddval.semibold.ttf"),
-    NTSomicMedium: require("../../assets/fonts/NTSomic-Medium.ttf"),
+    NTSomiBold: require("../../assets/fonts/NTSomic-Bold.ttf"),
+    NTSomic: require("../../assets/fonts/NTSomic-Regular.ttf"),
   });
   if (!fontsLoaded) {
     return null;
@@ -16,16 +17,12 @@ function HowToPlayScreen() {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>I AM A DEVELOPER</Text>
-      <View style={{ padding: 1 }}></View>
       <ScrollView>
         <Text style={styles.text}>
-          “I am a developer” is an interactive entertainment app designed to
-          simulate the journey of becoming a successful developer. Users start
-          from infancy and progress through various life stages, learning
-          subjects ranging from mathematics, literature, etc. to modern
-          programming languages, frameworks, or other IT fields. The game
-          operates on a time scale where each year in the game equates to 12
-          minutes in real life, offering a dynamic and engaging experience.
+          <Text style={styles.indexText}>“I am a developer”</Text> is an
+          interactive entertainment app designed to simulate the journey of
+          becoming a successful developer. Players progress through life stages,
+          with each in-game year equal to 12 real-life minutes.
         </Text>
         <Text></Text>
         <Text style={styles.text}>
@@ -36,22 +33,18 @@ function HowToPlayScreen() {
         </Text>
         <Text></Text>
         <Text style={styles.text}>
-          You will have to go through educational levels corresponding to
-          real-life educational levels such as primary school, secondary school,
-          high school and university. Make smart decisions to manage your
-          finances, balance your health and happiness, and social interactions.
-          At age 18, you will receive $10,000 from your parents and must make
-          important life decisions such as education, employment, housing,
-          relationships, etc. Various features are unlocked when the player
-          reaches a certain age or fulfills certain conditions, adding depth to
-          the gaming experience.
+          As the main character, players go through educational levels like
+          primary, secondary, high school, and university, making decisions
+          about finances, health, and relationships. At 18, they receive $10,000
+          from parents, facing crucial choices about education, employment, and
+          housing.
         </Text>
         <Text></Text>
         <Text style={styles.text}>
           In the game, random situations or unexpected accidents will appear
-          that will be detrimental to you. Therefore, always make sure that the
-          attributes are always balanced at a level that is not too low.
-          Especially health, if your health drops to 0, the game will end.
+          that will be detrimental to players. Therefore, always make sure that
+          the attributes are always balanced at a level that is not too low.
+          Especially health, if your health drops to 0, the game is over.
         </Text>
         <Text></Text>
         <Text style={styles.text}>
@@ -59,14 +52,14 @@ function HowToPlayScreen() {
         </Text>
         <Text style={styles.text}>
           <Ionicons name="heart" color={Colors.redHealth} size={15} />{" "}
-          <Text style={{ fontWeight: "bold" }}>Heart</Text> - Measures your
-          physical health. This is affected by your diet, age and activities.
-          Your health will decline as you age, no ones lives forever
+          <Text style={styles.indexText}>Heart</Text> - Measures your physical
+          health. This is affected by your diet, age and activities. Your health
+          will decline as you age, no ones lives forever
         </Text>
         <Text></Text>
         <Text style={styles.text}>
           <Ionicons name="happy" color={Colors.yellowHappiness} size={15} />{" "}
-          <Text style={{ fontWeight: "bold" }}>Happiness</Text> - Measures your
+          <Text style={styles.indexText}>Happiness</Text> - Measures your
           happiness, reduced by events and over working. You will commit suicide
           if your happiness is zero for too long. Let's take part in outside
           activities and interact with friends to increase happiness.
@@ -74,23 +67,23 @@ function HowToPlayScreen() {
         <Text></Text>
         <Text style={styles.text}>
           <FontAwesome6 name="brain" color={Colors.blueIQ} size={15} />{" "}
-          <Text style={{ fontWeight: "bold" }}>IQ</Text> - Measure your
-          intelligence level. If you have a high level of intelligence you can
-          study or do the jobs you want. Intelligence levels can decrease as you
-          get older or work in unsuitable jobs.
+          <Text style={styles.indexText}>IQ</Text> - Measure your intelligence
+          level. If you have a high level of intelligence you can study or do
+          the jobs you want. Intelligence levels can decrease as you get older
+          or work in unsuitable jobs.
         </Text>
         <Text></Text>
         <Text style={styles.text}>
           <Ionicons name="cash" color={Colors.lightGreen} size={15} />{" "}
-          <Text style={{ fontWeight: "bold" }}>Money</Text> - Shows how much
-          money you own. Only money you own can be directly used for actions.
+          <Text style={styles.indexText}>Money</Text> - Shows how much money you
+          own. Only money you own can be directly used for actions.
         </Text>
         <Text></Text>
         <Text style={styles.text}>
           <MaterialIcons name="savings" color={"pink"} size={17} />{" "}
-          <Text style={{ fontWeight: "bold" }}>Saving Money</Text> - Shows how
-          much money is in your bank. While the money in your bank cannot be
-          directly used, it will generate interest.
+          <Text style={styles.indexText}>Saving Money</Text> - Shows how much
+          money is in your bank. While the money in your bank cannot be directly
+          used, it will generate interest.
         </Text>
       </ScrollView>
     </View>
@@ -101,17 +94,23 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-    padding: 15,
+    paddingHorizontal: 20,
+    marginBottom: 20,
   },
   header: {
     fontFamily: "Oddval",
     color: "white",
-    fontSize: 30,
+    fontSize: 35,
   },
   text: {
-    fontFamily: "NTSomicMedium",
+    fontFamily: "NTSomic",
     color: "white",
-    fontSize: 18,
+    fontSize: 15,
+  },
+
+  indexText: {
+    fontFamily: "NTSomicBold",
+    fontSize: 17,
   },
 });
 
