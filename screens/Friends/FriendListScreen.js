b4934658a9280de67b1b66b954bd848543b86c3a
mhquang <mhquang03@gmail.com>
2024-04-30 13:50:51 +0700

    Update friend

--- screens/Friends/FriendListScreen.js
+++ screens/Friends/FriendListScreen.js
@@ -1,25 +1,35 @@
 import { View, StyleSheet } from "react-native";
 import { friends } from "../../data/friends/dummy-friends";
+import { useContext } from "react";
+import { AuthContext } from "../../store/auth-context";
+
 import Title from "../../components/ui/Title";
 import FriendItem from "../../components/ui/items/FriendItem";
 
 function FriendListScreen() {
+  const authCtx = useContext(AuthContext);
+  const userAge = authCtx.userData?.age;
+  const friendsList = authCtx.userData?.friends;
   return (
     <View style={styles.rootContainer}>
       <Title>People around you</Title>
       {friends.map((friend, index) => {
-        return (
-          <FriendItem
-            key={index}
-            name={friend.name}
-            hobbies={friend.hobbies}
-            age={friend.age}
-            gender={friend.gender}
-            health={friend.health}
-            iq={friend.iq}
-            happiness={friend.happiness}
-          />
-        );
+        const ageGap = Math.abs(userAge - friend.age);
+        const isAddFriend = friendsList.includes(friend.name);
+        if (ageGap <= 5 || isAddFriend) {
+          return (
+            <FriendItem
+              key={index}
+              name={friend.name}
+              hobbies={friend.hobbies}
+              age={friend.age}
+              gender={friend.gender}
+              health={friend.health}
+              iq={friend.iq}
+              happiness={friend.happiness}
+            />
+          );
+        }
       })}
     </View>
   );
